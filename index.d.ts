d3e8e64c620ceab244eac0054d85867159d269cf
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-08-05 22:49:46 -0700

    wip

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,3 @@
-import { Base } from "javascript-plugin-architecture-with-typescript-definitions";
 import fetch from "node-fetch";
 
 import { RequestInterface } from "./plugins/request/index.js";
@@ -6,8 +5,7 @@ import { RequestInterface } from "./plugins/request/index.js";
 import "./api-versions-types/github.com";
 
 export namespace Octokit {
-  interface Options<TVersion extends keyof Octokit.ApiVersions = "github.com">
-    extends Base.Options {
+  interface Options<TVersion extends keyof Octokit.ApiVersions = "github.com"> {
     /**
      * GitHub API Version. Defaults to "api.github.com"
      */
@@ -72,15 +70,127 @@ export namespace Octokit {
 
   interface ApiVersions {}
 }
+
 export declare class Octokit<
   TVersion extends keyof Octokit.ApiVersions = "github.com"
 > {
-  constructor(options: Octokit.Options<TVersion>);
+  /**
+   * Pass one or multiple plugin functions to extend the `Octokit` class.
+   * The instance of the new class will be extended with any keys returned by the passed plugins.
+   * Pass one argument per plugin function.
+   *
+   * ```js
+   * export function helloWorld() {
+   *   return {
+   *     helloWorld () {
+   *       console.log('Hello world!');
+   *     }
+   *   };
+   * }
+   *
+   * const MyBase = Base.withPlugins([helloWorld]);
+   * const base = new MyBase();
+   * base.helloWorld(); // `base.helloWorld` is typed as function
+   * ```
+   */
+  static withPlugins<
+    Class extends ClassWithPlugins,
+    Plugins extends [Plugin, ...Plugin[]]
+  >(
+    this: Class,
+    plugins: Plugins
+  ): Class & {
+    plugins: [...Class["plugins"], ...Plugins];
+  } & Constructor<UnionToIntersection<ReturnTypeOf<Plugins>>>;
 
+  /**
+   * Set defaults for the constructor
+   *
+   * ```js
+   * const MyBase = Base.withDefaults({ version: '1.0.0', otherDefault: 'value' });
+   * const base = new MyBase({ option: 'value' }); // `version` option is not required
+   * base.options // typed as `{ version: string, otherDefault: string, option: string }`
+   * ```
+   * @remarks
+   * Ideally, we would want to make this infinitely recursive: allowing any number of
+   * .withDefaults({ ... }).withDefaults({ ... }).withDefaults({ ... }).withDefaults({ ... })...
+   * However, we don't see a clean way in today's TypeScript syntax to do so.
+   * We instead artificially limit accurate type inference to just three levels,
+   * since real users are not likely to go past that.
+   * @see https://github.com/gr2m/javascript-plugin-architecture-with-typescript-definitions/pull/57
+   */
+  static withDefaults<
+    PredefinedOptionsOne,
+    ClassOne extends Constructor<Base<Octokit.Options & PredefinedOptionsOne>> &
+      ClassWithPlugins
+  >(
+    this: ClassOne,
+    defaults: PredefinedOptionsOne
+  ): ConstructorRequiringOptionsIfNeeded<ClassOne, PredefinedOptionsOne> & {
+    withDefaults<ClassTwo, PredefinedOptionsTwo>(
+      this: ClassTwo,
+      defaults: PredefinedOptionsTwo
+    ): ConstructorRequiringOptionsIfNeeded<
+      ClassOne & ClassTwo,
+      PredefinedOptionsOne & PredefinedOptionsTwo
+    > & {
+      withDefaults<ClassThree, PredefinedOptionsThree>(
+        this: ClassThree,
+        defaults: PredefinedOptionsThree
+      ): ConstructorRequiringOptionsIfNeeded<
+        ClassOne & ClassTwo & ClassThree,
+        PredefinedOptionsOne & PredefinedOptionsTwo & PredefinedOptionsThree
+      > &
+        ClassOne &
+        ClassTwo &
+        ClassThree;
+    } & ClassOne &
+      ClassTwo;
+  } & ClassOne;
+
+  /**
+   * list of plugins that will be applied to all instances
+   */
+  static plugins: Plugin[];
+
+  /**
+   * list of default options that will be applied to all instances
+   */
+  static defaults: {};
+
+  /**
+   * options passed to the constructor as constructor defaults
+   */
   options: Octokit.Options<TVersion>;
+
+  constructor(options: Octokit.Options<TVersion>);
 
   request: RequestInterface<TVersion>;
 }
+
+export type ExtendOctokitWith<
+  OctokitClass extends AbstractOctokit,
+  OctokitExtensions extends Extensions
+> = OctokitClass &
+  ConstructorRequiringOptionsIfNeeded<
+    OctokitClass & ApplyPlugins<OctokitExtensions["plugins"]>,
+    OrObject<OctokitClass["options"], unknown>
+  > &
+  ApplyPlugins<OctokitExtensions["plugins"]> & {
+    defaults: OrObject<OctokitExtensions["defaults"], undefined>;
+  };
+
+export declare type Plugin = (
+  instance: Octokit,
+  options: Octokit.Options
+) => ApiExtension | void;
+
+declare type ApiExtension = {
+  [key: string]: unknown;
+};
+
+// helpers
+declare type Constructor<T> = new (...args: any[]) => T;
 
 /**
  * @author https://stackoverflow.com/users/2887218/jcalz
@@ -99,22 +209,14 @@ declare type ReturnTypeOf<T extends AnyFunction | AnyFunction[]> =
     ? UnionToIntersection<Exclude<ReturnType<T[number]>, void>>
     : never;
 
-type Extensions = {
-  defaults?: {};
-  plugins?: Plugin[];
+type ClassWithPlugins = Constructor<any> & {
+  plugins: Plugin[];
 };
-
-type OrObject<T, Extender> = T extends Extender ? {} : T;
-
-type ApplyPlugins<Plugins extends Plugin[] | undefined> =
-  Plugins extends Plugin[]
-    ? UnionToIntersection<ReturnType<Plugins[number]>>
-    : {};
 
 type RemainingRequirements<PredefinedOptions> =
   keyof PredefinedOptions extends never
-    ? Base.Options
-    : Omit<Base.Options, keyof PredefinedOptions>;
+    ? Octokit.Options
+    : Omit<Octokit.Options, keyof PredefinedOptions>;
 
 type NonOptionalKeys<Obj> = {
   [K in keyof Obj]: {} extends Pick<Obj, K> ? undefined : K;
@@ -123,9 +225,9 @@ type NonOptionalKeys<Obj> = {
 type RequiredIfRemaining<PredefinedOptions, NowProvided> = NonOptionalKeys<
   RemainingRequirements<PredefinedOptions>
 > extends undefined
-  ? [(Partial<Base.Options> & NowProvided)?]
+  ? [(Partial<Octokit.Options> & NowProvided)?]
   : [
-      Partial<Base.Options> &
+      Partial<Octokit.Options> &
         RemainingRequirements<PredefinedOptions> &
         NowProvided
     ];
@@ -140,29 +242,19 @@ type ConstructorRequiringOptionsIfNeeded<Class, PredefinedOptions> = {
   };
 };
 
-declare type ApiExtension = {
-  [key: string]: unknown;
+type Extensions = {
+  defaults?: {};
+  plugins?: Plugin[];
 };
 
-export declare type Plugin = (
-  instance: Base,
-  options: Base.Options
-) => ApiExtension | void;
+type OrObject<T, Extender> = T extends Extender ? {} : T;
+
+type ApplyPlugins<Plugins extends Plugin[] | undefined> =
+  Plugins extends Plugin[]
+    ? UnionToIntersection<ReturnType<Plugins[number]>>
+    : {};
 
 declare class AbstractOctokit {
   constructor(options: any);
-
   options: any;
 }
-
-export type ExtendBaseWith<
-  OctokitClass extends AbstractOctokit,
-  BaseExtensions extends Extensions
-> = OctokitClass &
-  ConstructorRequiringOptionsIfNeeded<
-    OctokitClass & ApplyPlugins<BaseExtensions["plugins"]>,
-    OrObject<OctokitClass["options"], unknown>
-  > &
-  ApplyPlugins<BaseExtensions["plugins"]> & {
-    defaults: OrObject<BaseExtensions["defaults"], undefined>;
-  };
