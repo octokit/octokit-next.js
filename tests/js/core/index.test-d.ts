18c1abcd291c202aa6dd1f349bb1297b7fc4cbaf
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-08-15 13:08:36 -0700

    move test from `packages/types/index.test-d.ts` to `tests/js/core/index.test-d.ts`

--- tests/js/core/index.test-d.ts
+++ tests/js/core/index.test-d.ts
@@ -1,13 +1,31 @@
 import { expectType } from "tsd";
 
-import { Octokit } from "./index.js";
+import { Octokit, Plugin } from "@octokit-next/core";
 
 export async function test() {
   const octokit = new Octokit();
+
+  // @ts-expect-error - unknown properties cannot be used
+  octokit.unknown;
+
+  expectType<Plugin[]>(Octokit.plugins);
 
   const rootResponse = await octokit.request("GET /");
   expectType<string>(rootResponse.data.emojis_url);
 
   const emojisResponse = await octokit.request("GET /emojis");
   expectType<unknown>(emojisResponse.data);
+
+  const OctokitWithEmptyDefaults = Octokit.withDefaults({
+    // there should be no required options
+  });
+
+  // instances of subclasses retain Octokit's methods
+  const OctokitWithDefaultsAndPlugins = OctokitWithEmptyDefaults.withPlugins([
+    () => {},
+  ]);
+  const octokitWithDefaultsAndPlugins = new OctokitWithDefaultsAndPlugins();
+
+  const emojisResponse2 = octokitWithDefaultsAndPlugins.request("GET /");
+  expectType<string>(rootResponse.data.emojis_url);
 }
