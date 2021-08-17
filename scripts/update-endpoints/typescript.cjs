29f1d4e0fadf465b34a85fb61303f83f75270364
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-08-17 12:39:06 -0700

    automation: generate types for `types-rest-api` package

---
+++ scripts/update-endpoints/typescript.cjs
@@ -0,0 +1,80 @@
+const { readFileSync, writeFileSync } = require("fs");
+const { resolve } = require("path");
+
+const Handlebars = require("handlebars");
+const prettier = require("prettier");
+const sortKeys = require("sort-keys");
+
+const ENDPOINTS = require("./generated/endpoints.json");
+const ENDPOINTS_PATH = resolve(
+  __dirname,
+  "../../packages/types-rest-api/index.d.ts"
+);
+const ENDPOINTS_TEMPLATE_PATH = resolve(
+  __dirname,
+  "templates/endpoints.ts.template"
+);
+
+const template = Handlebars.compile(
+  readFileSync(ENDPOINTS_TEMPLATE_PATH, "utf8")
+);
+
+const endpointsByRoute = {};
+
+run();
+
+async function run() {
+  for (const endpoint of ENDPOINTS) {
+    if (endpoint.renamed) continue;
+
+    const route = `${endpoint.method} ${endpoint.url}`;
+
+    // The root endpoint types are set by default
+    if (route === "GET /") continue;
+
+    endpointsByRoute[route] = {
+      method: endpoint.method.toLowerCase(),
+      url: toOpenApiUrl(endpoint),
+      requiredPreview: (endpoint.previews[0] || {}).name,
+      documentationUrl: endpoint.documentationUrl,
+    };
+
+    // handle deprecated URL parameters
+    for (const parameter of endpoint.parameters) {
+      if (!parameter.deprecated || parameter.in !== "PATH") continue;
+      const { alias, name } = parameter;
+      const deprecatedRoute = route.replace(
+        new RegExp(`\\{${alias}\\}`),
+        `{${name}}`
+      );
+
+      endpointsByRoute[deprecatedRoute] = Object.assign(
+        {},
+        endpointsByRoute[route],
+        {
+          deprecated: `"${name}" is now "${alias}"`,
+        }
+      );
+    }
+  }
+
+  const result = template({
+    endpointsByRoute: sortKeys(endpointsByRoute, { deep: true }),
+  });
+
+  writeFileSync(
+    ENDPOINTS_PATH,
+    prettier.format(result, { parser: "typescript" })
+  );
+  console.log(`${ENDPOINTS_PATH} updated.`);
+}
+
+function toOpenApiUrl(endpoint) {
+  return (
+    endpoint.url
+      // stecial case for "Upload a release asset": remove ":origin" prefix
+      .replace(/^\{origin\}/, "")
+      // remove query parameters
+      .replace(/\{?\?.*$/, "")
+  );
+}
