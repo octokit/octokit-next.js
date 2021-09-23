65d335d21f8f1a042fcc966ee394f568ccec8f94
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-09-22 17:55:31 -0700

    test: adapt for new type system

--- tests/js/ghes-3.0-class/index.test-d.ts
+++ tests/js/ghes-3.0-class/index.test-d.ts
@@ -2,25 +2,32 @@ import { expectType } from "tsd";
 
 import { OctokitGhes30 } from "./index.js";
 
-export async function test() {
-  new OctokitGhes30({});
+export async function plugin(octokit: OctokitGhes30) {
+  const rootResponse = await octokit.request("GET /");
+  expectType<string>(rootResponse.data.emojis_url);
+  expectType<string>(rootResponse.headers["x-github-enterprise-version"]);
 
-  const octokit = new OctokitGhes30({
-    baseUrl: "https://github.acme-inc.com/api/v3",
-  });
+  // @ts-expect-error - `GET /orgs/{org}/audit-log` was added in GHES 3.1 and does not exist in GHES 3.0
+  octokit.request("GET /orgs/{org}/audit-log");
+
+  expectType<"ghes-3.0">(octokit.options.version);
+}
+
+export async function test() {
+  const octokit = new OctokitGhes30();
+  expectType<"ghes-3.0">(octokit.options.version);
 
   const rootResponse = await octokit.request("GET /");
   expectType<string>(rootResponse.data.emojis_url);
 
   expectType<string>(rootResponse.headers["x-github-enterprise-version"]);
 
-  // @ ts-expect-error - `GET /orgs/{org}/audit-log` was added in GHES 3.1 and does not exist in GHES 3.0
-
+  // @ts-expect-error - `GET /orgs/{org}/audit-log` was added in GHES 3.1 and does not exist in GHES 3.0
   await octokit.request("GET /orgs/{org}/audit-log", {
     org: "octokit",
   });
 
-  const response = await octokit.request("GET /orgs/{org}/audit-log", {
+  await octokit.request("GET /orgs/{org}/audit-log", {
     org: "octokit",
     request: {
       version: "ghes-3.1",
