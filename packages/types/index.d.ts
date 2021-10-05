c95105e2782b51a48a3e569d5093c9d67cecbcf7
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-10-04 22:31:12 -0700

    types(endpoint): fixies

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -101,11 +101,11 @@ export namespace Octokit {
     /**
      * Avoid setting `headers.accept`, use `mediaType.{format|previews}` option instead.
      */
-    accept: string;
+    accept?: string;
     /**
      * `user-agent` is set do a default and can be overwritten as needed.
      */
-    "user-agent": string;
+    "user-agent"?: string;
     /**
      * Use `authorization` to send authenticated request, remember `token ` / `bearer ` prefixes. Example: `token 1234567890abcdef1234567890abcdef12345678`
      */
