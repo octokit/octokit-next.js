d46e4a13db6bc7bf1da02c0acbbb2d4ac76c627e
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2022-09-09 15:57:18 -0700

    left todos

--- tests/ts/auth/test.ts
+++ tests/ts/auth/test.ts
@@ -44,6 +44,6 @@ export async function test() {
     authStrategy: createCallbackAuth,
   });
 
-  // TODO: @ts-expect-error - auth is required to be set to `{ callback }`
+  // @ts-expect-error - auth is required to be set to `{ callback }`
   new OctokitWithCallbackAuth();
 }
